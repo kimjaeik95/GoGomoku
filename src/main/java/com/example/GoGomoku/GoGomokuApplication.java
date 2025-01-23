c1ccd340cc1f8bda31ed13a52fd9475386a34c2f
kimjaeik95 <wodlrdlsla@naver.com>
2025-01-23 21:16:51 +0900

    first commit

---
+++ src/main/java/com/example/GoGomoku/GoGomokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoGomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGomokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGomokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoGomoku/GoGomokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoGomoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGomokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
